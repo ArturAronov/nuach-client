ef816f2aa20ede2be625bd33a8de61e680bcef69
Artur Aronov <unionco@icloud.com>
2025-06-22 03:22:38 +0800

    add demo page

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,21 @@
+export default function Demo() {
+  return (
+    <main>
+      <div className="flex gap-5">
+        <button>default button</button>
+        <button className="button-primary">primary button</button>
+        <button className="button-secondary">secondary button</button>
+        <button className="button-neutral">neutral button</button>
+        <button className="button-warning">warning button</button>
+        <button className="button-error">error button</button>
+      </div>
+
+      <form>
+        <label>label for text input</label>
+        <input type="text" />
+        <label>label for text area</label>
+        <textarea />
+      </form>
+    </main>
+  );
+}
